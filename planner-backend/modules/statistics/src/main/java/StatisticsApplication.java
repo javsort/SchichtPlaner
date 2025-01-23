eab8f4eb47307a602737876419abb9739ae14ac3
javsort <javierortega336@gmail.com>
2025-01-23 17:54:57 +0100

    Added initial pom.xml files for 2 modules

---
+++ planner-backend/modules/statistics/src/main/java/StatisticsApplication.java
@@ -0,0 +1,10 @@
+package com.LIT.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class StatisticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsApplication.class, args);
+    }
+}
