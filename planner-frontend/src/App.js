9362e504bba713224a2db31b038ea8896ff6f7dd
nadaelhaddad23 <nadaaelhaddad23@gmail.com>
2025-01-27 22:48:13 +0100

    Added RegisterPage component and redirection logic

--- planner-frontend/src/App.js
+++ planner-frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Import for React 18 and newer
+import ReactDOM from 'react-dom/client';  // React 18
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';  // Global styles
+import Login from './pages/Login';  // Import the Login component correctly
+import Register from './pages/Register';  // Import the Register component
 import AdminDashboard from './pages/AdminDashboard';
 import CreateShift from './pages/CreateShift';
 import AddEmployee from './pages/AddEmployee';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AdminDashboard />} /> {/* Default route */}
+        <Route path="/" element={<Login />} />  {/* Default route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />  {/* Register route */}
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/create-shift" element={<CreateShift />} />
         <Route path="/add-employee" element={<AddEmployee />} />
@@ -28,3 +33,4 @@ root.render(
 );
 
 export default App;
+
