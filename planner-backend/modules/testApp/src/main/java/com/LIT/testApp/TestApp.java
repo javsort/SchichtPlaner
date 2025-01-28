31a40abd9a017e79c0563dae26e9c0945f2635a4
javsort <javierortega336@gmail.com>
2025-01-28 15:51:22 +0100

    Are you actually kidding me

--- planner-backend/modules/testApp/src/main/java/com/LIT/testApp/TestApp.java
+++ planner-backend/modules/testApp/src/main/java/com/LIT/testApp/TestApp.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApp {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mariadb.jdbc.Driver");
+        /*try {
+            Class.forName("org.mariadb.jdbc.Driver");
             System.out.println("MariaDB Driver Loaded Successfully!");
         } catch (ClassNotFoundException e) {
             System.err.println("Failed to load MariaDB driver!");
             e.printStackTrace();
-        }
+        }*/
 
         SpringApplication.run(TestApp.class, args);
     }
