319835046a0b98a3f4fa5683069418fbac1ecea4
javsort <javierortega336@gmail.com>
2025-01-28 16:23:58 +0100

    Properly plugged in Database, connecting w the rest and moving on

--- schichtconfig/automation-scripts/jumpstart-backend.sh
+++ schichtconfig/automation-scripts/jumpstart-backend.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-java -jar /app/testApp-1.0-SNAPSHOT.jar
-# && java -jar /app/scheduler-1.0-SNAPSHOT.jar   # Runs on 8080 - 8081
+java -jar /app/testApp-1.0-SNAPSHOT.jar && java -jar /app/scheduler-1.0-SNAPSHOT.jar   # Runs on 8080 - 8081
