762e4734f9ff3ccc718b0d4fb0af5bde523753ac
Qviti <giorgiikandelaki@gmail.com>
2025-02-02 19:38:13 +0100

    changes to rolerepository

--- planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/RoleRepository.java
+++ planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/RoleRepository.java
@@ -1,14 +1,13 @@
-package com.LIT.scheduler.model.repository;
+// package com.LIT.scheduler.model.repository;
 
+// //import com.LIT.scheduler.model.entity.Role;
+// import com.LIT.auth.model.entity.Role;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import java.util.Optional;
 
-//import com.LIT.scheduler.model.entity.Role;
-import com.LIT.auth.model.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
+// public interface RoleRepository extends JpaRepository<Role, Long> {
+//     Optional<Role> findByName(String name);
+// }
 
 
 
