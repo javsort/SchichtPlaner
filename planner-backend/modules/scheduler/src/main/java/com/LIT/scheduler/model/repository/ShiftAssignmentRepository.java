33b33d8ad02a6946003f699a238abfd3cf32e4a2
Qviti <giorgiikandelaki@gmail.com>
2025-02-03 18:18:47 +0100

    slight changes to scheduler repository files

--- planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/ShiftAssignmentRepository.java
+++ planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/ShiftAssignmentRepository.java
@@ -1,16 +1,15 @@
 package com.LIT.scheduler.model.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import com.LIT.scheduler.model.entity.ShiftAssignment;
 
 public interface ShiftAssignmentRepository extends JpaRepository<ShiftAssignment, Long> {
-    @Query("SELECT sa FROM ShiftAssignment sa WHERE sa.user.id = ?1")
+
+    @Query("SELECT sa FROM ShiftAssignment sa WHERE sa.userId = ?1")
     List<ShiftAssignment> findByUserId(Long userId);
 
     @Query("SELECT sa FROM ShiftAssignment sa WHERE sa.shift.id = ?1")
-    List<ShiftAssignment> findByShift(Long shift);
+    List<ShiftAssignment> findByShiftId(Long shiftId);
 }
--- planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/ShiftRepository.java
+++ planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/model/repository/ShiftRepository.java
@@ -1,11 +1,8 @@
 package com.LIT.scheduler.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.LIT.scheduler.model.entity.Shift;
 
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
-
-    
-    //List<Shift> findByTitleContainingIgnoreCase(String title);
+    //basic CRUD
 }
