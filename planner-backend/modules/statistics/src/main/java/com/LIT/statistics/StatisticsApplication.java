d5838312c9bb8abf60fcdf7b40e5a1c524599feb
javsort <javierortega336@gmail.com>
2025-02-18 17:17:52 +0100

    Integrating statistics starts now...

--- planner-backend/modules/statistics/src/main/java/com/LIT/statistics/StatisticsApplication.java
+++ planner-backend/modules/statistics/src/main/java/com/LIT/statistics/StatisticsApplication.java
@@ -3,6 +3,7 @@ package com.LIT.statistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class StatisticsApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatisticsApplication.class, args);
---
+++ planner-backend/modules/statistics/src/main/java/com/LIT/statistics/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.LIT.statistics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    // Since logicGate checks entrances, disable sec for this module
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .formLogin(login -> login.disable())
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
