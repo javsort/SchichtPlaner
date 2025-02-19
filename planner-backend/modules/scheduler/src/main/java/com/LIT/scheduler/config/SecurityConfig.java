368d17d966e3c556f302c9ff039168a50faf53bb
javsort <javierortega336@gmail.com>
2025-02-19 17:53:25 +0100

    Fixed security configs for all non-authorization modules

--- planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/config/SecurityConfig.java
+++ planner-backend/modules/scheduler/src/main/java/com/LIT/scheduler/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.LIT.scheduler.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     // Since logicGate checks entrances, disable sec for this module
     @Bean
--- planner-backend/modules/statistics/src/main/java/com/LIT/statistics/config/SecurityConfig.java
+++ planner-backend/modules/statistics/src/main/java/com/LIT/statistics/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.LIT.statistics.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     // Since logicGate checks entrances, disable sec for this module
     @Bean
